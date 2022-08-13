4c738d650ac73b24e2a376689d1ac32a0a2eb6b3
Killian Sheriff <ksheriff@mit.edu>
2022-08-12 18:49:36 -0700

    updates

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(exclude=["tests.*", "tests", "figs", "examples"]),
     author="Killian Sheriff",
     author_email="ksheriff@mit.edu",
-    description="Python package to easily run sbatch jobs.",
+    description="Sbatchpy is a python package allowing easy sbatch job script creation and submissions on hpc clusters, directly from python.",
     license="MIT",
     keywords=["sbatch", "slurm", "python", "hpc"],
     url="https://github.com/killiansheriff/sbatchpy",
