462f33191d4a8c7f3987453b94859ada90f44e57
Killian Sheriff <ksheriff@mit.edu>
2023-08-11 22:12:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,7 @@
-import subprocess
-
 from setuptools import find_packages, setup
-
-version = (
-    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
-    .stdout.decode("utf-8")
-    .strip()
-)
-
-if "-" in version:
-    # when not on tag, git describe outputs: "1.3.3-22-gdf81228"
-    # pip has gotten strict with version numbers
-    # so change it to: "1.3.3+22.git.gdf81228"
-    # See: https://peps.python.org/pep-0440/#local-version-segments
-    v, i, s = version.split("-")
-    version = v + "+" + i + ".git." + s
-
 setup(
     name="sbatchpy",
-    version=version,
+    version=0.0.2,
     packages=find_packages(exclude=["tests.*", "tests", "figs", "examples"]),
     author="Killian Sheriff",
     author_email="ksheriff@mit.edu",
